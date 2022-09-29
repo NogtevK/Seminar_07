633245be7f8d3c86074a9bd2d8da7fa523d66ba3
Kirill Nogtev <nogtev.kirill@yandex.ru>
2022-09-29 18:12:02 +0300

    Task 46 complete. test ok

--- Ex_1/Program.cs
+++ Ex_1/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 46: Задайте двумерный массив размером m×n,
+заполненный случайными целыми числами.
+m = 3, n = 4.
+1 4 8 19
+5 -2 33 -2
+77 3 8 1 
+*/
+void Print(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+
+    }
+}
+int m = 3, n = 4;
+int[,] mass = new int[m, n];
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        //Console.Write(i + "-" + j + ",");
+        mass[i, j] = new Random().Next(0, 10);
+    }
+    Console.WriteLine();
+}
+Print(mass);
+Console.WriteLine();
+Print(mass);
