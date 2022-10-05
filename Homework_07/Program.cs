d6be7af37a7881f4c1b6af3bccd251b5c8dcfd25
Kirill Nogtev <nogtev.kirill@yandex.ru>
2022-10-05 15:12:11 +0400

    task 47 complete/test ok/commit&push

--- Homework_07/Program.cs
+++ Homework_07/Program.cs
@@ -6,4 +6,25 @@ m = 3, n = 4.
 8 7,8 -7,1 9
  */
 
- 
+void Print(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(Math.Round(arr[i, j], 2) + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+int m = 3, n = 4;
+double[,] mass = new double[m, n];
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        mass[i, j] = new Random().NextDouble() * 100;
+    }
+}
+Print(mass);
+Console.WriteLine();
